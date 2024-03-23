6159b8192d627c1f09c65bc12057c51015a60cdf
marwannour1 <marwannour20022016@gmail.com>
2024-03-23 21:06:47 +0200

    constant V1

--- test.cpp
+++ test.cpp
@@ -10,18 +10,18 @@ enum TokenType
 {
     STRING_LITERAL, // 3
     PREPROCESSOR,   // 6
+    COMMENT,        // 8
     KEYWORD,        // 0
     IDENTIFIER,     // 1
-    CONSTANT,       // 2
     OPERATOR,       // 4
+    CONSTANT,       // 2
     PUNCTUATOR,     // 5
     WHITESPACE,     // 7
-    COMMENT,        // 8
     UNKNOWN         // 9
 };
 
 std::map<TokenType, std::string> tokenTypeToString = {
-    {STRING_LITERAL, "STRING_LITERAL"},
+     {STRING_LITERAL, "STRING_LITERAL"},
     {KEYWORD, "KEYWORD"},
     {IDENTIFIER, "IDENTIFIER"},
     {CONSTANT, "CONSTANT"},
@@ -30,73 +30,67 @@ std::map<TokenType, std::string> tokenTypeToString = {
     {PREPROCESSOR, "PREPROCESSOR"},
     {WHITESPACE, "WHITESPACE"},
     {COMMENT, "COMMENT"},
-    {UNKNOWN, "UNKNOWN"}};
+    {UNKNOWN, "UNKNOWN"}
+};
 
 std::map<TokenType, std::regex> tokenPatterns = {
     {STRING_LITERAL, std::regex("\"([^\"\\\\]|\\\\[\\s\\S])*\"")},
     {KEYWORD, std::regex("\\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|inline|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b")},
     {IDENTIFIER, std::regex("[a-zA-Z_][a-zA-Z0-9_]*")},
-    {CONSTANT, std::regex("\\b([0-9]+)\\b")},
-    {OPERATOR, std::regex("[\\+\\-\\*/%&|^<>!]?=|[<>]=?|[\\+\\-\\*/%&|^<>!]")},
+   {CONSTANT, std::regex("(((\\+-)*\\+|(-\\+)*-?)(0|[1-9][0-9]*)(\\.([0-9]*))?((e|E)(\\+|-)?[0-9]+)?)")},
+    {OPERATOR, std::regex("[+\\-\\*/%&|^<>!=]?=|[<>]=?|[+\\-*/%&|^<>!]")},
     {PUNCTUATOR, std::regex("[\\[\\]\\(\\)\\{\\}\\.,;]")},
-    {PREPROCESSOR, std::regex("^#[a-zA-Z]+")},
+    {PREPROCESSOR, std::regex("^[[:blank:]]*#[[:blank:]]*[A-Za-z_][A-Za-z0-9_]*[[:blank:]]*<[^>]*>?")},
     {WHITESPACE, std::regex("\\s+")},
-    {COMMENT, std::regex("//.*|/\\*.*?\\*/")}};
+    {COMMENT, std::regex("//.*|/\\*[^*]*\\*+(?:[^/*][^*]*\\*+)*/")}
+};
 
-void lexicalAnalyzer(const std::string &filename)
-{
+
+void lexicalAnalyzer(const std::string& filename) {
     std::ifstream file(filename);
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
         return;
     }
 
     // Combined regex pattern
     std::regex combinedPattern = std::regex(
-        "\"([^\"\\\\]|\\\\[\\s\\S])*\""                                                                                                                                                                                         // STRING_LITERAL
+        "\"([^\"\\\\]|\\\\[\\s\\S])*\"" // STRING_LITERAL
         "|\\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|inline|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b" // KEYWORD
-        "|[a-zA-Z_][a-zA-Z0-9_]*"                                                                                                                                                                                               // IDENTIFIER
-        "|\\b([0-9]+)\\b"                                                                                                                                                                                                       // CONSTANT
-        "|[\\+\\-\\*/%&|^<>!]?=|[<>]=?|[\\+\\-\\*/%&|^<>!]"                                                                                                                                                                     // OPERATOR
-        "|[\\[\\]\\(\\)\\{\\}\\.,;]"                                                                                                                                                                                            // PUNCTUATOR
-        "|^#[a-zA-Z]+"                                                                                                                                                                                                          // PREPROCESSOR
-        "|\\s+"                                                                                                                                                                                                                 // WHITESPACE
-        "|//.*|/\\*.*?\\*/"                                                                                                                                                                                                     // COMMENT
+        "|^[[:blank:]]*#[[:blank:]]*[A-Za-z_][A-Za-z0-9_]*[[:blank:]]*<[^>]*>?" // PREPROCESSOR
+        "|[a-zA-Z_][a-zA-Z0-9_]*" // IDENTIFIER
+        "|(((\\+-)*\\+|(-\\+)*-?)(0|[1-9][0-9]*)(\\.([0-9]*))?((e|E)(\\+|-)?[0-9]+)?)" // CONSTANT
+        "|//.*|/\\*.*?\\*/" // COMMENT
+        "|[\\+\\-\\*/%&|^<>!]?=|[<>]=?|[\\+\\-\\*/%&|^<>!]" // OPERATOR
+        "|[\\[\\]\\(\\)\\{\\}\\.,;]" // PUNCTUATOR
+        "|\\s+" // WHITESPACE
+
     );
 
     std::string line;
-    while (std::getline(file, line))
-    {
+    while (std::getline(file, line)) {
         std::vector<std::string> matches;
         std::sregex_iterator it(line.begin(), line.end(), combinedPattern);
         std::sregex_iterator reg_end;
-        for (; it != reg_end; ++it)
-        {
+        for (; it != reg_end; ++it) {
             matches.push_back((*it).str()); // Store each match in the vector
         }
 
         // Iterate over matches and determine their types
-        for (const auto &match : matches)
-        {
+        for (const auto& match : matches) {
             bool typeFound = false;
-            for (const auto &pair : tokenPatterns)
-            {
-                if (std::regex_match(match, pair.second))
-                {
-
-                    if (pair.first == WHITESPACE || pair.first == COMMENT)
-                    {
+            for (const auto& pair : tokenPatterns) {
+                if (std::regex_match(match, pair.second)) {
+                    if (pair.first == WHITESPACE || pair.first == COMMENT) {
                         typeFound = true;
-                        continue;
-                    }
+						continue;
+					}
                     std::cout << "Token: " << match << ", Type: " << tokenTypeToString[pair.first] << std::endl;
                     typeFound = true;
                     break;
                 }
             }
-            if (!typeFound)
-            {
+            if (!typeFound) {
                 std::cout << "Unknown token: " << match << std::endl;
             }
         }
@@ -107,7 +101,7 @@ int main()
 {
     lexicalAnalyzer("test.txt");
 
-    // cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
     /*Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     CompilerForC::MainForm form;
